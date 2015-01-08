36a5a474f49c9947893ff8f3986c78e0e3749a99
keram79 <marek.kreul@gmail.com>
2015-01-08 20:32:48 +0100

    Improving makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -14,19 +14,30 @@ files="zshrc zsh vimrc vim tmux.conf"    # list of files/folders to symlink in h
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+if [ ! -d $olddir ]
+	then
+		printf "Creating $olddir for backup of any existing dotfiles in ~ ...\t"
+		mkdir -p $olddir
+		printf "done.\n"
+	fi
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
+if [ "$PWD"!="$dir" ]
+	then
+	printf "We are in $PWD right now, changing to the $dir directory ...\t"
+	cd $dir
+	printf "done\n"
+fi
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+
+printf "Moving any existing dotfiles from ~ to $olddir\n\n"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    if [ -a ~/.$file ]
+	then
+        printf "Moving ~/.$file to $olddir/... \t"
+    	mv ~/.$file $olddir/ && printf "done.\n"
+    fi
+    printf "Creating symlink to $file in home directory ...\t"
+    ln -s $dir/$file ~/.$file && printf "done.\n"
 done
