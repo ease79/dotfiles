1f89d512279bc1b1331ef59a2254520bafa560cd
Marek <marek@posteo.de>
2015-01-25 15:31:58 +0100

    fixes a bug in the routine checking if the script was executed from within the dotfiles directory

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 ############################
-# .make.sh
+# makesymlinks.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir="$HOME/dotfiles"                    # dotfiles directory
+olddir="$HOME/dotfiles_old"             # old dotfiles backup directory
 #files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
 files="zshrc zsh vimrc vim tmux.conf"    # list of files/folders to symlink in homedir
 
@@ -22,15 +22,14 @@ if [ ! -d $olddir ]
 	fi
 
 # change to the dotfiles directory
-if [ "$PWD"!="$dir" ]
-	then
-	printf "We are in $PWD right now, changing to the $dir directory ...\t"
-	cd $dir
-	printf "done\n"
+if [ "$PWD" != "$dir" ]
+    then
+	    printf "We are in $PWD right now, changing to the $dir directory ...\t"
+    	cd $dir
+	    printf "done\n"
 fi
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-
 printf "Moving any existing dotfiles from ~ to $olddir\n\n"
 for file in $files; do
     if [ -a ~/.$file ]
